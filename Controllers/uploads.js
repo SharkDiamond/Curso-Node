83821297e40e66642d90a76cf84d785b083b975d
Osnet Computer Oficina <gabrielarispe24@gmail.com>
2022-01-21 17:47:43 -0500

    Se trabajo en donde se guarda los archivos

--- Controllers/uploads.js
+++ Controllers/uploads.js
@@ -1,15 +1,36 @@
+const path=require('path');
+const {v4:uuidv4}=require('uuid');
 
 
 const cargarArchivo=(req,res)=>{
 
-    
-    console.log(req.files);
-    res.json({
-        msg:"subiendo archivo..."
+  
+    if (!req.files || Object.keys(req.files || !req.files.archivo).length === 0) return res.status(400).json({msg:'No hay archivos que subir'});
+  
+    // The name of the input field (i.e. "archivo") is used to retrieve the uploaded file
+   const  {archivo} = req.files;
 
-    })
+    const nombreCortado=archivo.name.split('.');
+    //EXTENSION DEL ARCHIVO 
+    const extension=nombreCortado[nombreCortado.length-1];
+    //EXTENCIONES PERMITIDAS
+    const permitExtension=['png','yml','txt'];
+    //VALIDAR EXTENSIONES
+    if (!permitExtension.includes(extension)) return res.status(400).json({msg:`La extension ${extension} no es permitida, ${permitExtension}`}).end();
+    //GENERANDO NOMBRE UNICO
+    const nameTemp=`${archivo.name}-${uuidv4()}.${extension}`;
 
+   const uploadPath =path.join( __dirname, '../uploads/', nameTemp);
+  
+    // MOVIENDO EL ARCHIVO
+    archivo.mv(uploadPath, function(err) {
+        //EN DADO CASO DE QUE HAYA UN ERRIR
+        if (err) return res.status(500).send({err});
+  
+        res.json({msg:'File uploaded!'}).end();
 
+    });
+   
 }
 
 
